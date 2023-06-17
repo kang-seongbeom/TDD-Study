0776de91c1aa5cf93031f6f295052b57da3eb75c
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2023-06-17 23:15:07 +0900

    feat(complete) : webmvc 학습 테스트

---
+++ src/main/java/com/example/tddstudy/learning/LearningDto.java
@@ -0,0 +1,12 @@
+package com.example.tddstudy.learning;
+
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Builder
+public class LearningResponseDto {
+    long id;
+    String name;
+}
